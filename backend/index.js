6acdd6bf1a102336b6b4f7f18b7463b36af3cb99
Rekha Verma <vrekha116@gmail.com>
2026-07-05 00:56:18 +0530

    cors code

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,12 @@
 const express  = require('express');
+const cors = require('cors');
 require('./db/config');
 const User = require('./db/User');
 const app = express();
+
 app.use(express.json());
+app.use(cors());
+
 app.post("/register", async (req,resp)=>{
     // resp.send("api is working");
     // resp.send(req.body);
