4519fa2edb8d28a90a29489081b8af4ac5b39733
Rekha Verma <vrekha116@gmail.com>
2026-07-05 00:13:41 +0530

    Database COnnection

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,23 @@
 const express  = require('express');
 
+const mongoose = require('mongoose');
+
 const app = express();
+
+const connectDB = async () => {
+    mongoose.connect("mongodb://localhost:27017/e-commerce");
+    const productSchema = new mongoose.Schema({
+        name: String,
+        price: Number,
+        description: String,
+        image: String
+    });
+    const Product = mongoose.model('Product', productSchema);
+    const data = await Product.find();
+    console.warn(data);
+}
+
+connectDB();
 
 app.get("/",(req,resp)=>{
     resp.send("Hello World");
